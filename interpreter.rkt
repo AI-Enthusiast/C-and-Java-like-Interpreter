c5b7480f008fc97ab2fbe92ad37e62a05992f1cb
Cormac <builderofthefuture@gmail.com>
2019-02-12 17:36:33 -0500

    Minor change
    added a space and it wants me to commit it

--- interpreter.rkt
+++ interpreter.rkt
@@ -26,3 +26,48 @@
 (define operator caddr)
 (define left-operand car)
 (define right-operand cadr)
+
+
+
+
+;;;;**********TESTING**********
+
+;; Performs a quick test to see if the test passed or failed and prints info about test if failure
+(define pass?
+  (lambda (actual expected)
+    (if (equal? actual expected)
+        (display 'Pass)
+        (donothing (display "Fail {actual} ") (display actual)      ; displays info about the failed test
+                   (display " != {expected} ") (display expected)))
+    (newline))) 
+
+;; This function does nothing but allows for all the displays of a failed test to occure without an error :)
+(define donothing ; there is probably a better way to do this but as this isn't part of the grade, ̄\_(ツ)_/̄
+  (lambda (a b c d)
+    '())) ; literly does nothing (like what I wish I was doing xD)
+
+;; Performs all the tests needed to prove the validity of the functions, I love this function 
+(define (test)
+
+  ;Example:
+  ;(diplay "Test #{test number} {test name}") (newline)
+  ;(pass? {actual} {expected})
+  ;(newline)
+  
+  (display "Test #1 mvalue") (newline)                                                ;Test mvalue
+  (pass? (mvalue '(3 + (4 / 2))) 5)                                                       ; 1/2
+  (pass? (mvalue '((3 * 2) + (4 / (2 % 3)))) 8)                                           ; 2/2
+  (newline)
+  
+  (display "Test 2 exp") (newline)                                                    ;Test start
+  (pass? (start "Test1.txt") '((var x) (= x 10) (var y (+ (* 3 x) 5))                     ; 1/1
+                                   (while (!= (% y x) 3) (= y (+ y 1)))
+                                   (if (> x y) (return x)
+                                       (if (> (* x x) y) (return (* x x))
+                                           (if (> (* x (+ x x)) y)
+                                               (return (* x (+ x x)))
+                                               (return (- y 1)))))))
+  (newline)
+
+  
+  ) ;left hanging for easy test addition
