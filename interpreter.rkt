773ca25ce170295761dc16de15fdac0434f343c9
Cormac <builderofthefuture@gmail.com>
2019-02-12 16:32:57 -0500

    Added testing documentation
    Added functions: pass?, donothing, & test. In which (test) will run all tests

--- interpreter.rkt
+++ interpreter.rkt
@@ -18,3 +18,27 @@
 (define operator caddr)
 (define left-operand car)
 (define right-operand cadr)
+
+
+
+
+;;;;**********TESTING**********
+
+;; Performs a quick test to see if the test passed or failed and prints info about test if failure
+(define pass?
+  (lambda (actual expected)
+    (if (equal? actual expected)
+        (display 'Pass)
+        (donothing (display "Fail {actual} ") (display actual)      ; displays info about the failed test
+                   (display " != {expected} ") (display expected)))
+    (newline))) 
+
+;; This function does nothing but allows for all the displays of a failed test to occure without an error :)
+(define donothing ; there is probably a better way to do this but as this isn't part of the grade, ̄\_(ツ)_/̄
+  (lambda (a b c d)
+    '())) ; literly does nothing (like what I wish I was doing xD)
+
+;; Performs all the tests needed to prove the validity of the functions, I love this function 
+(define (test)
+  (display "Test #1 exp") (newline)                                                ;Test exp
+  (pass? (exp ((3 * 2) + (4 / (2 % 3)))) 8)                                              ; 1/1
