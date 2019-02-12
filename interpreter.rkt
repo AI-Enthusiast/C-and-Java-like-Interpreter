af78d2427c58302bfbe85cf09ce64aa813f2135a
Caitlin Campbell <cac226@case.edu>
2019-02-12 17:11:25 -0500

    improved readability

--- interpreter.rkt
+++ interpreter.rkt
@@ -1,36 +1,39 @@
 #lang racket
 
-; Code a function that can take in expression of numbers and operators and return the value
-; e.g. (3 + (4 / 2))
-;      (1 + 2)
-; The operators are +, -, *, /, %, and division is integer division
+;; Code a function that can take in expression of numbers and operators and return the value
+;; e.g. (3 + (4 / 2))
+;;      (1 + 2)
+;; The operators are +, -, *, /, %, and division is integer division
 (define mvalue
   (lambda (exp s)
     (cond
-      [(null? exp) (error 'undefined "undefined expression")]
-      [(number? exp) exp]
+      [(null? exp)             (error 'undefined "undefined expression")]
+      [(number? exp)           exp]
       [(eq? (operator exp) '+) (+         (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
       [(eq? (operator exp) '-) (-         (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
       [(eq? (operator exp) '*) (*         (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
       [(eq? (operator exp) '/) (quotient  (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
-      [(eq? (operator exp) '%) (remainder (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))])))
+      [(eq? (operator exp) '%) (remainder (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
+      [else                    (error 'undefined "undefined expression")])))
 
-; Code a function that can take in an expression such as (5 < 2) and return true/false
-; ==, !=, <, >, <=, >=
-; &&, ||, !
+;; Code a function that can take in an expression such as (5 < 2) and return true/false
+;; Supports ==, !=, <, >, <=, >=, &&, ||, !
 (define mcondition
   (lambda (exp s) ; exp = expression, s = state
     (cond
+      ; null checking
       [(null? exp)                    (error 'undefined "undefined expression")]
       [(not (pair? exp))              exp]
       [(null? (bool_operator exp))    (mvalue exp s)]
       [(null? (left-operand exp))     (mvalue exp s)]
       [(null? (right-operand exp))    (mvalue exp s)]
 
+      ; condition checking (&&, ||, !)
       [(eq? (bool_operator exp) '||)  (or  (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
       [(eq? (bool_operator exp) '&&)  (and (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
       [(eq? (car exp) '!)             (not (mcondition (cdr exp) s))]
 
+      ; equality/inequality operator checking (==, !=, <, >, <=, >=)
       [(eq? (bool_operator exp) '==)  (eq? (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
       [(eq? (bool_operator exp) '!=)  (not (eq? (mcondition (left-operand exp) s) (mcondition (right-operand exp) s)))]
       [(eq? (bool_operator exp) '<)   (< (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
@@ -38,7 +41,8 @@
       [(eq? (bool_operator exp) '<=)  (<= (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
       [(eq? (bool_operator exp) '>=)  (>= (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
 
-      [else                           (mvalue exp s)])))
+      ; oh no
+      [else                           (error 'undefined "undefined expression")])))
 
 ; for mcondition
 (define bool_operator cadr)
