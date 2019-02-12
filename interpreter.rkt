a62821a3ced4b3a6da27c4d1dd6ed41ca7d6e53a
Caitlin Campbell <cac226@case.edu>
2019-02-12 16:30:56 -0500

    Began working on boolean expressions

--- interpreter.rkt
+++ interpreter.rkt
@@ -5,7 +5,7 @@
 ;      (1 + 2)
 ; The operators are +, -, *, /, %, and division is integer division
 (define mvalue
-  (lambda (exp)
+  (lambda (exp s)
     (cond
       [(null? exp) (error 'undefined "undefined expression")]
       [(number? exp) exp]
@@ -15,6 +15,19 @@
       [(eq? (operator exp) '/) (quotient  (mvalue (left-operand exp)) (mvalue (right-operand exp)))]
       [(eq? (operator exp) '%) (remainder (mvalue (left-operand exp)) (mvalue (right-operand exp)))])))
 
+; Code a function that can take in an expression such as (5 < 2) and return true/false
+; ==, !=, <, >, <=, >=
+; &&, ||, !
+(define mcondition
+  (lambda (exp s) ; exp = expression, s = state
+    (cond
+      [(null? exp) (error 'undefined "undefined expression")]
+      [(eq? (bool_operator exp) '||) (or (mcondition ()])))
+
+; for mcondition
+(define bool_operator caddr)
+
+; for mvalue
 (define operator caddr)
 (define left-operand car)
 (define right-operand cadr)
