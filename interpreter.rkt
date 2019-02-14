a9841f01369e88631b1b066bed4b020cbcb33011
Cormac <builderofthefuture@gmail.com>
2019-02-13 22:39:12 -0500

    Comments, Func Names, Prefix, & Testing
    - Added and removed comments for better comprehension
    - Changed fuction names to aline w/ style guide
    - Changed m-value & m-condition to prefix notation as how the code is parsed into a tree by (start)
    - Created a TODO testing list and tested m-condition
    - minor error fix in m-condition for !opporator

--- interpreter.rkt
+++ interpreter.rkt
@@ -3,13 +3,8 @@
 ;;;; EECS 345
 ;;;; Group #7: Shanti Polara, Catlin ...., Cormac Dacker
 
-;;TODO: Var decleration, assignment (after decleration, error otherwise), return, if & while statements
+;;TODO: Var decleration, assignment (after decleration, error otherwise), return
 
-
-; Code a function that can take in expression of numbers and operators and return the value
-; e.g. (3 + (4 / 2))
-;      (1 + 2)
-; The operators are +, -, *, /, %, and division is integer division
 (require "simpleParser.rkt") ; loads simpleParser.rkt, which itself loads lex.rkt
 
 
@@ -26,57 +21,56 @@
 
 
 
-; TODO: change to precondition
 ;; Code a function that can take in expression of numbers and operators and return the value
 ;; e.g. (+ 3 (/ 4 2))
 ;;      (+ 1 2)
 ;; The operators are +, -, *, /, %, and division is integer division
-(define mvalue
+(define m-value
   (lambda (exp s)
     (cond
       [(null? exp)             (error 'undefined "undefined expression")]
       [(number? exp)           exp]
-      [(eq? (operator exp) '+) (+         (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
-      [(eq? (operator exp) '-) (-         (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
-      [(eq? (operator exp) '*) (*         (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
-      [(eq? (operator exp) '/) (quotient  (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
-      [(eq? (operator exp) '%) (remainder (mvalue (left-operand exp) s) (mvalue (right-operand exp) s))]
+      [(eq? (operator exp) '+) (+         (m-value (left-operand exp) s) (m-value (right-operand exp) s))]
+      [(eq? (operator exp) '-) (-         (m-value (left-operand exp) s) (m-value (right-operand exp) s))]
+      [(eq? (operator exp) '*) (*         (m-value (left-operand exp) s) (m-value (right-operand exp) s))]
+      [(eq? (operator exp) '/) (quotient  (m-value (left-operand exp) s) (m-value (right-operand exp) s))]
+      [(eq? (operator exp) '%) (remainder (m-value (left-operand exp) s) (m-value (right-operand exp) s))]
       [else                    (error 'undefined "undefined expression")])))
 
-;; Code a function that can take in an expression such as (5 < 2) and return true/false
+;; Code a function that can take in an expression such as (< 5 2) and return true/false
 ;; Supports ==, !=, <, >, <=, >=, &&, ||, !
-(define mcondition
+(define m-condition
   (lambda (exp s) ; exp = expression, s = state
     (cond
       ; null checking
       [(null? exp)                    (error 'undefined "undefined expression")]
       [(not (pair? exp))              exp]
-      [(null? (operator exp))    (mvalue exp s)]
-      [(null? (operator exp))     (mvalue exp s)]
-      [(null? (operator exp))    (mvalue exp s)]
+      [(null? (operator exp))    (m-value exp s)]
+      [(null? (operator exp))     (m-value exp s)]
+      [(null? (operator exp))    (m-value exp s)]
 
       ; condition checking (&&, ||, !)
-      [(eq? (operator exp) '||)  (or  (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
-      [(eq? (operator exp) '&&)  (and (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
-      [(eq? (car exp) '!)             (not (mcondition (cdr exp) s))]
+      [(eq? (operator exp) '||)  (or  (m-condition (left-operand exp) s) (m-condition (right-operand exp) s))]
+      [(eq? (operator exp) '&&)  (and (m-condition (left-operand exp) s) (m-condition (right-operand exp) s))]
+      [(eq? (car exp) '!)             (not (m-condition (left-operand exp) s))]
 
       ; equality/inequality operator checking (==, !=, <, >, <=, >=)
-      [(eq? (operator exp) '==)  (eq? (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
-      [(eq? (operator exp) '!=)  (not (eq? (mcondition (left-operand exp) s) (mcondition (right-operand exp) s)))]
-      [(eq? (operator exp) '<)   (< (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
-      [(eq? (operator exp) '>)   (> (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
-      [(eq? (operator exp) '<=)  (<= (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
-      [(eq? (operator exp) '>=)  (>= (mcondition (left-operand exp) s) (mcondition (right-operand exp) s))]
+      [(eq? (operator exp) '==)  (eq? (m-condition (left-operand exp) s) (m-condition (right-operand exp) s))]
+      [(eq? (operator exp) '!=)  (not (eq? (m-condition (left-operand exp) s) (m-condition (right-operand exp) s)))]
+      [(eq? (operator exp) '<)   (< (m-condition (left-operand exp) s) (m-condition (right-operand exp) s))]
+      [(eq? (operator exp) '>)   (> (m-condition (left-operand exp) s) (m-condition (right-operand exp) s))]
+      [(eq? (operator exp) '<=)  (<= (m-condition (left-operand exp) s) (m-condition (right-operand exp) s))]
+      [(eq? (operator exp) '>=)  (>= (m-condition (left-operand exp) s) (m-condition (right-operand exp) s))]
 
       ; oh no
       [else                           (error 'undefined "undefined expression")])))
 
 ;; implementing if statement
-(define mifstatement
+(define m-if-statement
   (lambda (exp s)
     (cond
       [(null? exp) (error 'undefined "undefined expression")]
-      [(mcondition (loop-condition exp) s) (m-what-type (loop-body exp) s)]
+      [(m-condition (loop-condition exp) s) (m-what-type (loop-body exp) s)]
       [(not (null? (else-statement exp)))  (m-what-type (else-statement exp) s)])))
 
 #|;; implementing while loop
@@ -84,7 +78,7 @@
   (lambda (exp s)
     (cond
       [(null? exp) (error 'undefined "undefined expression")]
-      [(mcondition (loop-condition exp) s) (whileloop exp s #|TODO: SOMETHING TO UPDATE THE STATE IN HERE!!!|#)])))|#
+      [(m-condition (loop-condition exp) s) (whileloop exp s #|TODO: SOMETHING TO UPDATE THE STATE IN HERE!!!|#)])))|#
 
 ;; Abstration
 ;; for if statements
@@ -95,11 +89,11 @@
 
 ;; for value operations
 (define left-operand cadr)
-; for mvalue
-(define operator cadr)
+; for m-value
+(define operator car)
 (define right-operand caddr)
 
-; (5 + 2 <= 7)
+; (5 + 2 <= 7) ;can we remove these? or test for them?
 ; ((5 + 2) <= 7)
 
 (define vars car)
@@ -203,7 +197,14 @@ m-remove - removes a variable and it's value from state, returns updated state
       [(eq? a (car lis)) (cdr lis)]
       [else (cons (car lis) (remove a (cdr lis)))])))
 
-
+;; takes an expression
+;; returns it as if it where in C/Java
+(define m-return
+  (lambda (exp)
+    (cond
+      [(eq? exp #t) "True"]
+      [(eq? exp #f) "False"]
+      [else exp])))
 
 
 ;;;;**********TESTING**********
@@ -217,11 +218,13 @@ m-remove - removes a variable and it's value from state, returns updated state
                    (display " != {expected} ") (display expected)))
     (newline)))
 
-;; This function does nothing but allows for all the displays of a failed test to occure without an error :)
-(define donothing ; there is probably a better way to do this but as this isn't part of the grade, ̄\_(ツ)_/̄
+;; This function does nothing but allows for all the displays of a failed test to occure without an error
+(define donothing ; there is probably a better way to do this but  ̄\_(ツ)_/̄
   (lambda (a b c d)
-    '())) ; literly does nothing (like what I wish I was doing xD)
+    '())) ; literly does nothing (like what I wish I was doing)
 
+
+; TODO: Test (m-what-type, m-if-statment, whileloop
 ;; Performs all the tests needed to prove the validity of the functions, I love this function
 (define (test)
 
@@ -230,13 +233,9 @@ m-remove - removes a variable and it's value from state, returns updated state
   ;(pass? {actual} {expected})
   ;(newline)
 
-  (display "Test #1 mvalue") (newline)                                                ;Test mvalue
-  ;(pass? (mvalue '(3 + (4 / 2))) 5)                                                       ; 1/2
-  ;(pass? (mvalue '((3 * 2) + (4 / (2 % 3)))) 8)                                           ; 2/2
-  (newline)
-  
-  (display "Test #2 exp") (newline)                                                   ;Test start
-  (pass? (start "Test1.txt") '((var x) (= x 10) (var y (+ (* 3 x) 5))                     ; 1/1
+  ;checks the code is parsed into a tree as exprected
+  (display "Test #1 Start") (newline)                                                 ;Test start
+  (pass? (start "Test1.txt") '((var x) (= x 10) (var y (+ (* 3 x) 5))                           ; 1/1
                                    (while (!= (% y x) 3) (= y (+ y 1)))
                                    (if (> x y) (return x)
                                        (if (> (* x x) y) (return (* x x))
@@ -244,42 +243,77 @@ m-remove - removes a variable and it's value from state, returns updated state
                                                (return (* x (+ x x)))
                                                (return (- y 1)))))))
   (newline)
+  
+  ;checks math opperations perform correctly
+  ;TODO: replace 'temp with an s state
+  (display "Test #2 m-value") (newline)                                               ;Test m-value
+  (pass? (m-value '(+ 3 (/ 4 2)) 'temp) 5)                                                      ; 1/2
+  (pass? (m-value '(+ (* 3 2) (/ 4 (% 2 3))) 'temp) 8)                                          ; 2/2
+  (newline)
 
-
+  ;boolean ooporators for var assignments and conditions in if & while statements
+  ;TODO: replace 'temp with an s state
+  (display "Test #3 m-condition") (newline)                                           ;Test m-condition
+  (pass? (m-condition '(== 1 1) 'temp) #t)                                                      ; 1/23
+  (pass? (m-condition '(== 1 0) 'temp) #f)                                                      ; 2/23
+  (pass? (m-condition '(!= 1 1) 'temp) #f)                                                      ; 3/23
+  (pass? (m-condition '(!= 1 0) 'temp) #t)                                                      ; 4/23
+  (pass? (m-condition '(> 1 1) 'temp) #f)                                                       ; 5/23
+  (pass? (m-condition '(> 1 0) 'temp) #t)                                                       ; 6/23
+  (pass? (m-condition '(> 0 1) 'temp) #f)                                                       ; 7/23
+  (pass? (m-condition '(< 1 1) 'temp) #f)                                                       ; 8/23
+  (pass? (m-condition '(< 1 0) 'temp) #f)                                                       ; 9/23
+  (pass? (m-condition '(< 0 1) 'temp) #t)                                                       ; 10/23
+  (pass? (m-condition '(>= 1 1) 'temp) #t)                                                      ; 11/23
+  (pass? (m-condition '(>= 1 0) 'temp) #t)                                                      ; 12/23
+  (pass? (m-condition '(>= 0 1) 'temp) #f)                                                      ; 13/23
+  (pass? (m-condition '(<= 1 1) 'temp) #t)                                                      ; 14/23
+  (pass? (m-condition '(<= 1 0) 'temp) #f)                                                      ; 15/23
+  (pass? (m-condition '(<= 0 1) 'temp) #t)                                                      ; 16/23
+  (pass? (m-condition '(&& #t #t) 'temp) #t)                                                    ; 17/23
+  (pass? (m-condition '(&& #t #f) 'temp) #f)                                                    ; 18/23
+  (pass? (m-condition '(|| #t #t) 'temp) #t)                                                    ; 19/23
+  (pass? (m-condition '(|| #t #f) 'temp) #t)                                                    ; 20/23
+  (pass? (m-condition '(|| #f #f) 'temp) #f)                                                    ; 21/23
+  (pass? (m-condition '(! #t) 'temp) #f)                                                        ; 22/23
+  (pass? (m-condition '(! #f) 'temp) #t)                                                        ; 23/23
+  (newline)
+  
   ;lookup variable's value in the state
-  (display "Test #3 m-lookup") (newline)
-  (pass? (m-lookup 'a '((a b c d)(2 5 6 7))) 2)
-  (pass? (m-lookup 'c '((a b c d)(2 5 6 7))) 6)
-  (pass? (m-lookup 'd '((a b c d)(2 5 6 7))) 7)
-  (pass? (m-lookup 'd '()) "error, does not exist")
-  (pass? (m-lookup 's '(()())) "error, does not exist")
+  (display "Test #4 m-lookup") (newline)                                              ;Test m-lookup
+  (pass? (m-lookup 'a '((a b c d)(2 5 6 7))) 2)                                                 ; 1/5
+  (pass? (m-lookup 'c '((a b c d)(2 5 6 7))) 6)                                                 ; 2/5
+  (pass? (m-lookup 'd '((a b c d)(2 5 6 7))) 7)                                                 ; 3/5
+  (pass? (m-lookup 'd '()) "error, does not exist")                                             ; 4/5
+  (pass? (m-lookup 's '(()())) "error, does not exist")                                         ; 5/5
   (newline)
 
   ;update variable's value in the state
-  (display "Test #4 m-update") (newline)
-  (pass? (m-update 's 3 '((a b c d)(2 5 6 7))) "error")
-  (pass? (m-update 'a 3 '((a b c d)(2 5 6 7))) '((a b c d)(3 5 6 7)))
-  (pass? (m-update 'b 21 '((a b c d)(2 5 6 7))) '((a b c d)(2 21 6 7)))
-  (pass? (m-update 'd 1 '((a b c d)(2 5 6 7)))  '((a b c d)(2 5 6 1)))
-  (pass? (m-update 'a 0 '()) "error")
-  (pass? (m-update 'a 0 '(()())) "error")
+  (display "Test #5 m-update") (newline)                                              ;Test m-update
+  (pass? (m-update 's 3 '((a b c d)(2 5 6 7))) "error")                                         ; 1/6
+  (pass? (m-update 'a 3 '((a b c d)(2 5 6 7))) '((a b c d)(3 5 6 7)))                           ; 2/6
+  (pass? (m-update 'b 21 '((a b c d)(2 5 6 7))) '((a b c d)(2 21 6 7)))                         ; 3/6
+  (pass? (m-update 'd 1 '((a b c d)(2 5 6 7)))  '((a b c d)(2 5 6 1)))                          ; 4/6
+  (pass? (m-update 'a 0 '()) "error")                                                           ; 5/6
+  (pass? (m-update 'a 0 '(()())) "error")                                                       ; 6/6
   (newline)
 
   ;add a variable to the state
-  (display "Test #5 m-add") (newline)
-  (pass? (m-add 's '()) '((s)("init")))
-  (pass? (m-add 's '(()())) '((s)("init")))
-  (pass? (m-add 's '((a)(2))) '((s a)("init" 2)))
-  (pass? (m-add 's '((a b c)(3 4 5))) '((s a b c)("init" 3 4 5)))
+  (display "Test #6 m-add") (newline)                                                 ;Test m-add
+  (pass? (m-add 's '()) '((s)("init")))                                                         ; 1/4
+  (pass? (m-add 's '(()())) '((s)("init")))                                                     ; 2/4
+  (pass? (m-add 's '((a)(2))) '((s a)("init" 2)))                                               ; 3/4
+  (pass? (m-add 's '((a b c)(3 4 5))) '((s a b c)("init" 3 4 5)))                               ; 4/4
   (newline)
 
   ;remove a variable from a state
-  (display "Test #6 m-remove") (newline)
-  (pass? (m-remove 'a '((a b c d)(2 5 6 7))) '((b c d)(5 6 7)))
-  (pass? (m-remove 'b '((a b c d)(2 5 6 7))) '((a c d)(2 6 7)))
-  (pass? (m-remove 'd '((a b c d)(2 5 6 7))) '((a b c)(2 5 6)))
-  (pass? (m-remove 'a '((b c d)(5 6 7))) "error")
-  (pass? (m-remove 'a '(()())) "error")
-  (pass? (m-remove 'a '()) "error")
-           
+  (display "Test #7 m-remove") (newline)                                              ;Test m-remove
+  (pass? (m-remove 'a '((a b c d)(2 5 6 7))) '((b c d)(5 6 7)))                                 ; 1/6
+  (pass? (m-remove 'b '((a b c d)(2 5 6 7))) '((a c d)(2 6 7)))                                 ; 2/6
+  (pass? (m-remove 'd '((a b c d)(2 5 6 7))) '((a b c)(2 5 6)))                                 ; 3/6
+  (pass? (m-remove 'a '((b c d)(5 6 7))) "error")                                               ; 4/6
+  (pass? (m-remove 'a '(()())) "error")                                                         ; 5/6
+  (pass? (m-remove 'a '()) "error")                                                             ; 6/6
+  (newline)
+
   ) ;left hanging for easy test addition
