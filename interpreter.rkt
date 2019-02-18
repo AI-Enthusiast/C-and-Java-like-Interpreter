cac3ba7fd5975413227d1592f89e1000e3c67395
Caitlin Campbell <cac226@case.edu>
2019-02-18 17:40:04 -0500

    fixed "true/false" capitalization

--- interpreter.rkt
+++ interpreter.rkt
@@ -274,12 +274,12 @@ m-remove - removes a variable and it's value from state, returns updated state
 (define m-return
   (lambda (exp s)
     (cond
-      [(eq?   exp #t) "True"]
-      [(eq?   exp #f) "False"]
+      [(eq?   exp #t) "true"]
+      [(eq?   exp #f) "false"]
       [(and (pair? exp) (am-i-boolean exp)) (m-return (m-condition exp s) s)]
       [(pair? exp)    (m-value exp s)]
-      [(eq? (m-value exp s) #t) "True"]
-      [(eq? (m-value exp s) #f) "False"]
+      [(eq? (m-value exp s) #t) "true"]
+      [(eq? (m-value exp s) #f) "false"]
       [else           (m-value exp s)])))
 
 ;;;;**********ABSTRACTION**********
@@ -459,10 +459,10 @@ m-remove - removes a variable and it's value from state, returns updated state
   ;;(pass? (run "Tests/p1.Test12.txt") "error") ;should error                                     ; 15/23
   ;;(pass? (run "Tests/p1.Test13.txt") "error") ;should error                                     ; 16/23
   ;;(pass? (run "Tests/p1.Test14.txt") "error") ;should error                                     ; 17/23
-  (pass? (run "Tests/p1.Test15.txt") "True")                                                    ; 18/23
+  (pass? (run "Tests/p1.Test15.txt") "true")                                                    ; 18/23
   (pass? (run "Tests/p1.Test16.txt") 100)                                                       ; 19/23
-  (pass? (run "Tests/p1.Test17.txt") "False")                                                   ; 20/23
-  (pass? (run "Tests/p1.Test18.txt") "True")                                                    ; 21/23
+  (pass? (run "Tests/p1.Test17.txt") "false")                                                   ; 20/23
+  (pass? (run "Tests/p1.Test18.txt") "true")                                                    ; 21/23
   (pass? (run "Tests/p1.Test19.txt") 128)                                                       ; 22/23
   (pass? (run "Tests/p1.Test20.txt") 12)                                                        ; 23/23
   (newline)
